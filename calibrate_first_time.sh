0a7e6fffcbfcadd58b1ccc222d7639e56c30589a
Achyut Boggaram <aboggaram@iunu.com>
2023-04-04 16:54:53 -0700

    bug fix

--- calibrate_first_time.sh
+++ calibrate_first_time.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is used to calibrate the first time the camera is used
 time python3 calib.py \
---calibration_settings calibration_settings.yaml \
+--settings calibration_settings.yaml \
 --mono \
 --stereo | tee calib.log
